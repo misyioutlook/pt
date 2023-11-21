74989af7b3a60620d92565411f5c38719706503f
licong <misyi.licong@gmail.com>
2023-11-21 17:08:42 +0800

    update

---
+++ install/raid0/mount.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]]; then
+  echo "请提供硬盘数量!"
+  exit 1
+fi
+
+param_count=$1
+chars=("b" "c" "d" "e" "f" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "q" "r" "s" "t" "u" "v" "w" "x" "y" "z" "A" "B" "C" "D" "E" "F" "G")
+
+if [[ $param_count -lt 1 || $param_count -gt 31 ]]; then
+  echo "参数个数必须在 1-31 范围内"
+  exit 1
+fi
+apt-get install mdadm -y
+
+selected_chars=("${chars[@]:0:param_count}")
+
+mdadm_command="mdadm -Cv /dev/md0 -l0 -n$param_count"
+
+for char in "${selected_chars[@]}"; do
+  mdadm_command+=" /dev/sd$char"1
+done
+
+echo "构建的 mdadm 命令为："
+echo "$mdadm_command"
+
+# 执行构建的 mdadm 命令
+eval "$mdadm_command"
+
+mkfs.ext4 /dev/md0
+
+mkdir /down
+
+mount /dev/md0 /down
+
+chmod 777 /down
+
+tune2fs -m 0 /dev/md0
+
+#读取md0的uuid
+md0_uuid=$(blkid -s UUID -o value /dev/md0)
+
+#写入到fstab启动文件
+echo "UUID=$md0_uuid /data ext4 defaults 0 0" >> /etc/fstab
